7bd250cbaf55e6c6dcabad3e353b64b4ddad5aad
oalado <vitoror16@gmail.com>
2025-12-11 12:28:36 -0300

    Exercicios 1 a 4 do Uri Judge

---
+++ URI/2 - AREA DE CIRCULO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uri1003 {
+    class Program {
+        static void Main(string[] args) {
+
+            double area, raio, pi = 3.14159;
+            
+            raio = double.Parse(Console.ReadLine());
+            area = pi * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4"));
+        }
+    }
+}
