f79af2cea29f2b95304db2d6d06a091b280ffbe6
oalado <vitoror16@gmail.com>
2025-12-17 19:56:29 -0300

    Exercicios 15 18 19 20 21 e 61 do Uri Judge

---
+++ URI/15 - DISTANCIA ENTRE DOIS PONTOS/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿using System; 
+using System.Globalization;
+class URI {
+
+    static void Main(string[] args) { 
+
+        double ponto1X, ponto1Y, ponto2X, ponto2Y, distancia;
+        
+        string [] vet1 = Console.ReadLine().Split(' ');
+
+        ponto1X = double.Parse(vet1[0], CultureInfo.InvariantCulture);
+        ponto1Y = double.Parse(vet1[1], CultureInfo.InvariantCulture);
+        
+         string [] vet2 = Console.ReadLine().Split(' ');
+        
+        ponto2X = double.Parse(vet2[0], CultureInfo.InvariantCulture);
+        ponto2Y = double.Parse(vet2[1], CultureInfo.InvariantCulture);
+
+        distancia = Math.Sqrt(Math.Pow
+        (ponto2X - ponto1X, 2) + Math.Pow(ponto2Y - ponto1Y, 2));
+     
+        Console.WriteLine(distancia.ToString("F4"));
+        
+
+    }
+
+}
---
+++ URI/19 - CONVERSAO DE TEMPO/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Globalization;
+
+namespace uri1017 {
+    class Program {
+        static void Main(string[] args) {
+        
+        int horas, velocidade, viagem;
+        double litros;
+        
+        horas = int.Parse(Console.ReadLine());
+        velocidade = int.Parse(Console.ReadLine());
+        viagem = horas * velocidade;
+        
+        litros = viagem / 12.0;
+        
+        Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+        
+
+        }
+    }
+}
