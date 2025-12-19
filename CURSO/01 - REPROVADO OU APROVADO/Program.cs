fd356a951f1f9de655367c4d73f9ce6e85df7abb
oalado <vitoror16@gmail.com>
2025-12-19 19:11:44 -0300

    Exercicio 36 do Uri Judge e Exercicio do Curso Udemy

---
+++ CURSO/01 - REPROVADO OU APROVADO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1003 {
+    class Program {
+        static void Main(string[] args) {
+
+            double int nota1, nota2, notaFinal;
+
+        double = Console.ReadLine(), CultureInfo.InvariantCulture);
+        nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            notaFinal = (nota1 + nota2);
+
+            if (notaFinal < 60.0) {
+                Console.WriteLine("REPROVADO");
+            }
+            else {
+                Console.WriteLine("APROVADO");
+            }
+
+            
+        }
+    }
+}
